39dc35f4b7ac0c1535c8dd57ecceaa46739d72fc
Bruno Weber de Castro <brunoweber500@gmail.com>
2021-08-31 18:48:41 -0300

    feature: cria e estiliza sessao de contato

---
+++ teste-dynamox/src/Components/Footer/index.js
@@ -0,0 +1,24 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
+import React from 'react';
+
+import { Section } from './styles.js';
+
+
+/* --------------------------------- EXPORTS ---------------------------------*/
+export default function Footer() {
+  return (
+    
+      <Section>
+        <p>Ficou com dúvida?</p>
+        <p>Nós entramos em contato com você</p>
+
+        <input type="text" id="fname" name="fname" placeholder="Como gostaria de ser chamado?" />
+        <input type="text" id="company" name="company" placeholder="Em qual empresa você trabalha?" />
+        <input type="text" id="email" name="email" placeholder="Digite aqui o seu email" />
+        <input type="text" id="phone" name="phone" placeholder="Qual o seu telefone?" />
+        
+        <button>ENVIAR</button>
+      </Section>
+    
+  );
+}
---
+++ teste-dynamox/src/Components/Footer/styles.js
@@ -0,0 +1,70 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
+import styled from 'styled-components';
+
+/* --------------------------------- EXPORTS ---------------------------------*/
+
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #263252;
+  padding: 40px;
+    
+  p {
+    font-style: normal;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 35px;
+    text-align: center;
+
+    color: #FFFFFF;
+    
+    margin: 0 0 30px;
+
+    :first-child{
+      margin-bottom: 0;
+    }
+  }
+
+  input {
+    max-width: 426px;
+    width: 90%;
+    height: 41px;
+    margin-bottom: 11px;
+
+    background: #F4F7FC;
+    border-radius: 5px;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 19px;
+
+    text-align: center;
+
+    color: #454545;
+  }
+
+  button{
+    margin-top: 27px;
+    margin-bottom: 40px;
+    width: 183px;
+    height: 39px;
+
+    background: #0165DB;
+    border-radius: 5px;
+    border-color: #0165DB;
+    border: none;
+    margin-left: auto;
+    margin-right: auto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 23px;
+
+    color: #FFFFFF;
+  }
+
+
+
+`;
